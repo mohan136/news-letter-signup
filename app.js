7f48701a4842dc7bfb0211b486bc1c67862101c8
mohan136 <83769660+mohan136@users.noreply.github.com>
2021-07-10 08:54:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.get("/", function(req,res){
-    res.sendFile(__dirname + "/signup.html");
+    res.sendFile(__dirname + "/index.html");
 });
 app.post("/", function(req,res){
     const firstName = req.body.fName;
